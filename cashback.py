cc28d5f1e3b860500e965fc8fafef0caaaba0f7b
Gabriel França <gabrielfrncsimao@gmail.com>
2026-06-15 01:00:48 -0300

    try/finally e ip no proxy

--- cashback.py
+++ cashback.py
@@ -34,15 +34,23 @@ def aplicacaoDesconto (compra, cupom, vip):
 
 @app.post("/calcular")
 async def calcular_cashback(dados: requisicaoCompra, request: Request): # o parametro dados recebe o corpo da requisição, e request da acesso aos dados brutos da requisição
-    ip = request.client.host #guarda o ip da requisição
+    
+    # Tenta pegar o IP real da nuvem; se não achar (testando local), usa o client.host
+    ip = request.headers.get("X-Forwarded-For", request.client.host)
+    # X-Forwarded-For pode retornar vários IPs separados por vírgula, pega o primeiro
+    ip = ip.split(",")[0].strip()
+    
     cashback = aplicacaoDesconto(dados.compra, dados.cupom, dados.vip)
     
     conexao = connection() #abre conexão para para executar o select
-    cursor = conexao.cursor() #abre o cursor para executar o select
-    cursor.execute("""INSERT INTO consultas_cashback(ip_usuario, vip, valor_compra, cupom, cashback_calculado) values (%s, %s, %s, %s, %s)""", (ip, dados.vip, dados.compra, dados.cupom, round(cashback, 2)))
-    conexao.commit() #confirma a operação no banco
-    cursor.close() #fecha o cursor após o uso
-    conexao.close() #fecha a conexão após o uso
+    try:
+        cursor = conexao.cursor() #abre o cursor para executar o select
+        cursor.execute("""INSERT INTO consultas_cashback(ip_usuario, vip, valor_compra, cupom, cashback_calculado) values (%s, %s, %s, %s, %s)""", (ip, dados.vip, dados.compra, dados.cupom, round(cashback, 2)))
+        conexao.commit() #confirma a operação no banco
+    
+    finally:
+        cursor.close() #fecha o cursor após o uso
+        conexao.close() #fecha a conexão após o uso
     
     return {
         "ip": ip,
@@ -54,14 +62,17 @@ async def calcular_cashback(dados: requisicaoCompra, request: Request): # o para
     
 @app.get("/historico")
 async def get_historico(request: Request): #função não preciso do corpo somente do request, só vai usar o ip de queme stá chamando
-    ip = request.client.host #guarda o ip da requisição
+    ip = request.headers.get("X-Forwarded-For", request.client.host)
+    ip = ip.split(",")[0].strip()
     
     conexao = connection()
-    cursor = conexao.cursor()
-    cursor.execute("""SELECT * FROM consultas_cashback where ip_usuario = %s ORDER BY data_hora_consulta""", (ip,))
-    registros = cursor.fetchall() # busca todos os resultados da query de uma vez e salvaa na variavel registros
-    cursor.close()
-    conexao.close()
+    try:
+        cursor = conexao.cursor()
+        cursor.execute("""SELECT * FROM consultas_cashback where ip_usuario = %s ORDER BY data_hora_consulta""", (ip,))
+        registros = cursor.fetchall() # busca todos os resultados da query de uma vez e salvaa na variavel registros
+    finally:
+        cursor.close()
+        conexao.close()
     
     return {
         "ip": ip,
--- conexao.py
+++ conexao.py
@@ -10,5 +10,6 @@ def connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")), # converte para o tipo int pq o que vem do .env é string
         cursorclass=pymysql.cursors.DictCursor #faz o cursor retornar os resultados como dicionario ao inves de duplas
     )
